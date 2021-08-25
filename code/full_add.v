ac55e3db0a5c677a03e628c1497d7bec2eb0617d
Frightwig <88388050+Frightwig@users.noreply.github.com>
2021-08-25 11:06:56 +0800

    Update full_add.v

--- code/full_add.v
+++ code/full_add.v
@@ -2,10 +2,10 @@
 //全加器模块
 module full_add
 (
-	input      cin,				// 进位输入
-	input		  a,					//	加数1
-	input 	  b,					//	加数2
-	output 	  sum,				//	和
+	input     cin,				// 进位输入
+	input	  a,				//加数1
+	input 	  b,				//加数2
+	output 	  sum,				//和
 	output	  cout				// 进位输出
 );
 
